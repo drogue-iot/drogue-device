06d53d4f39157c279834ab140096b4dbb6422b36
Bob McWhirter <bmcwhirt@redhat.com>
2021-01-28 13:24:54 -0500

    Bring examples into the repo.

---
+++ examples/stm32l4/iot01a/src/device.rs
@@ -0,0 +1,105 @@
+use stm32l4xx_hal::pac::{Interrupt, I2C2};
+use stm32l4xx_hal::gpio::{PA5, Output, PushPull, PC13, Input, PullUp, OpenDrain, AF4, Alternate, PB10, PB11, PD15, PullDown, Floating, PB14};
+use crate::button::{Button, ButtonEvent};
+use drogue_device::{
+    prelude::*,
+    synchronization::Mutex,
+    driver::{
+        sensor::hts221::{
+            Hts221,
+        },
+        led::SimpleLED,
+    },
+};
+use stm32l4xx_hal::i2c::I2c;
+use drogue_device::driver::sensor::hts221::Sensor;
+use drogue_device::driver::timer::Timer;
+
+type Ld1Actor = SimpleLED<MyDevice, PA5<Output<PushPull>>>;
+type Ld2Actor = SimpleLED<MyDevice, PB14<Output<PushPull>>>;
+type ButtonInterrupt = Button<PC13<Input<PullUp>>>;
+
+type I2cScl = PB10<Alternate<AF4, Output<OpenDrain>>>;
+type I2cSda = PB11<Alternate<AF4, Output<OpenDrain>>>;
+type I2cPeriph = I2c<I2C2, (I2cScl, I2cSda)>;
+type I2cActor = Mutex<MyDevice, I2cPeriph>;
+
+use drogue_device::hal::timer::stm32l4xx::Timer as McuTimer;
+use stm32l4xx_hal::pac::TIM15;
+use drogue_device::driver::led::Blinker;
+
+type Blinker1Actor = Blinker<MyDevice, PA5<Output<PushPull>>, McuTimer<TIM15>>;
+type Blinker2Actor = Blinker<MyDevice, PB14<Output<PushPull>>, McuTimer<TIM15>>;
+
+type TimerActor = Timer<MyDevice, McuTimer<TIM15>>;
+
+type Hts221Package = Hts221<MyDevice, PD15<Input<PullDown>>, I2cPeriph>;
+type Hts221Sensor = Sensor<MyDevice, I2cPeriph>;
+
+pub struct MyDevice {
+    pub ld1: ActorContext<MyDevice, Ld1Actor>,
+    pub ld2: ActorContext<MyDevice, Ld2Actor>,
+    pub blinker1: ActorContext<MyDevice, Blinker1Actor>,
+    pub blinker2: ActorContext<MyDevice, Blinker2Actor>,
+    pub button: InterruptContext<MyDevice, ButtonInterrupt>,
+    pub i2c: ActorContext<MyDevice, I2cActor>,
+    pub hts221: Hts221Package,
+    pub timer: InterruptContext<MyDevice, Timer<MyDevice, McuTimer<TIM15>>>,
+}
+
+impl Device for MyDevice {
+    fn mount(&'static mut self, bus: &EventBus<Self>, supervisor: &mut Supervisor) {
+        let ld1_addr = self.ld1.mount(bus, supervisor);
+        let ld2_addr = self.ld2.mount(bus, supervisor);
+
+        let blinker1_addr = self.blinker1.mount(bus, supervisor);
+        let blinker2_addr = self.blinker2.mount(bus, supervisor);
+
+        let i2c_addr = self.i2c.mount(bus, supervisor);
+        let hts221_addr = self.hts221.mount(bus, supervisor);
+        let timer_addr = self.timer.mount(bus, supervisor);
+
+        blinker1_addr.bind(&timer_addr);
+        blinker1_addr.bind(&ld1_addr);
+
+        blinker2_addr.bind(&timer_addr);
+        blinker2_addr.bind(&ld2_addr);
+
+
+        hts221_addr.bind(&i2c_addr);
+
+        let button_addr = self.button.mount(bus, supervisor);
+    }
+}
+
+pub struct ButtonToLed {
+    ld1: Address<MyDevice, Ld1Actor>,
+    hts221: Address<MyDevice, Hts221Sensor>,
+}
+
+impl ButtonToLed {
+    pub fn new(ld1: Address<MyDevice, Ld1Actor>, hts221: Address<MyDevice, Hts221Sensor>) -> Self {
+        Self {
+            ld1,
+            hts221,
+        }
+    }
+}
+
+/*
+impl Sink<ButtonEvent> for ButtonToLed {
+    fn notify(&self, message: ButtonEvent) {
+        match message {
+            ButtonEvent::Pressed => {
+                self.ld1.turn_on();
+                self.hts221.trigger_read_temperature();
+            }
+            ButtonEvent::Released => {
+                self.ld1.turn_off();
+            }
+        }
+    }
+}
+
+ */
+
---
+++ examples/stm32l4/iot01a/src/main.rs
@@ -0,0 +1,151 @@
+#![no_std]
+#![no_main]
+
+mod button;
+//mod led;
+mod device;
+//mod hts221;
+
+use cortex_m_rt::{entry, exception};
+use stm32l4xx_hal::{prelude::*, rcc::RccExt, stm32::Peripherals};
+
+use log::LevelFilter;
+use panic_rtt_target as _;
+use rtt_logger::RTTLogger;
+use rtt_target::rtt_init_print;
+
+use stm32l4xx_hal::gpio::{Edge, Input, Output, PullUp, PushPull, PA5, PC13};
+
+use button::Button;
+use device::MyDevice;
+
+use stm32l4xx_hal::pac::Interrupt::{EXTI15_10, TIM15};
+use drogue_device::{
+    prelude::*,
+    synchronization::Mutex,
+    driver::{
+        sensor::hts221::Hts221,
+        led::SimpleLED,
+    },
+};
+use stm32l4xx_hal::pac::{I2C2, RCC};
+use stm32l4xx_hal::i2c::I2c;
+use stm32l4xx_hal::time::Hertz;
+use cortex_m::peripheral::NVIC;
+use drogue_device::driver::timer::{
+    Timer,
+};
+use drogue_device::hal::timer::stm32l4xx::Timer as McuTimer;
+use drogue_device::driver::led::Blinker;
+use drogue_device::hal::Active;
+use drogue_device::domain::time::duration::Milliseconds;
+
+static LOGGER: RTTLogger = RTTLogger::new(LevelFilter::Debug);
+
+#[entry]
+fn main() -> ! {
+    //rtt_init_print!(BlockIfFull);
+    rtt_init_print!();
+    log::set_logger(&LOGGER).unwrap();
+    log::set_max_level(log::LevelFilter::Debug);
+    log::info!("Init");
+
+    let mut device = Peripherals::take().unwrap();
+
+    log::info!("initializing");
+    let mut flash = device.FLASH.constrain();
+    let mut rcc = device.RCC.constrain();
+    let mut pwr = device.PWR.constrain(&mut rcc.apb1r1);
+    let clocks = rcc
+        .cfgr
+        .sysclk(80.mhz())
+        .pclk1(80.mhz())
+        .pclk2(80.mhz())
+        .freeze(&mut flash.acr, &mut pwr);
+
+    let mut gpioa = device.GPIOA.split(&mut rcc.ahb2);
+    let mut gpiob = device.GPIOB.split(&mut rcc.ahb2);
+    let mut gpioc = device.GPIOC.split(&mut rcc.ahb2);
+    let mut gpiod = device.GPIOD.split(&mut rcc.ahb2);
+
+    // == LEDs ==
+
+    let ld1 = gpioa
+        .pa5
+        .into_push_pull_output(&mut gpioa.moder, &mut gpioa.otyper);
+
+    let ld1 = SimpleLED::new(ld1, Active::High);
+
+    let ld2 = gpiob
+        .pb14
+        .into_push_pull_output(&mut gpiob.moder, &mut gpiob.otyper);
+
+    let ld2 = SimpleLED::new(ld2, Active::High);
+
+    // == Blinker ==
+
+    let blinker1 = Blinker::new(Milliseconds(500u32));
+    let blinker2 = Blinker::new(Milliseconds(1000u32));
+
+    // == Button ==
+
+    let mut button = gpioc
+        .pc13
+        .into_pull_up_input(&mut gpioc.moder, &mut gpioc.pupdr);
+
+    button.make_interrupt_source(&mut device.SYSCFG, &mut rcc.apb2);
+    button.enable_interrupt(&mut device.EXTI);
+    button.trigger_on_edge(&mut device.EXTI, Edge::RISING_FALLING);
+
+    let button = Button::new(button);
+
+    // == i2c
+
+    let scl = gpiob
+        .pb10
+        .into_open_drain_output(&mut gpiob.moder, &mut gpiob.otyper)
+        .into_af4(&mut gpiob.moder, &mut gpiob.afrh);
+
+    let sda = gpiob.pb11
+        .into_open_drain_output(&mut gpiob.moder, &mut gpiob.otyper)
+        .into_af4(&mut gpiob.moder, &mut gpiob.afrh);
+
+    let i2c = I2c::i2c2(device.I2C2, (scl, sda), Hertz(100_000u32), clocks, &mut rcc.apb1r1);
+
+    let i2c = Mutex::new(i2c);
+
+    // == HTS221 ==
+
+    let mut ready = gpiod.pd15.into_pull_down_input(&mut gpiod.moder, &mut gpiod.pupdr);
+    //let mut ready = gpiod.pd15;
+    ready.enable_interrupt(&mut device.EXTI);
+    ready.make_interrupt_source(&mut device.SYSCFG, &mut rcc.apb2);
+    ready.trigger_on_edge(&mut device.EXTI, Edge::RISING);
+
+    let hts221 = Hts221::new(ready, EXTI15_10);
+
+    // == Timer ==
+
+    let mcu_timer = McuTimer::tim15( device.TIM15, clocks, &mut rcc.apb2);
+    let timer = Timer::new(mcu_timer );
+
+
+
+    // == Device ==
+
+    let device = MyDevice {
+        ld1: ActorContext::new(ld1).with_name("ld1"),
+        ld2: ActorContext::new(ld2).with_name("ld2"),
+        blinker1: ActorContext::new(blinker1) .with_name("blinker1"),
+        blinker2: ActorContext::new(blinker2) .with_name("blinker2"),
+
+        i2c: ActorContext::new(i2c).with_name("i2c"),
+        //hts221: ActorContext::new(hts221).with_name("hts221" ),
+        hts221,
+        button: InterruptContext::new(button, EXTI15_10).with_name("button"),
+        timer: InterruptContext::new( timer, TIM15 ).with_name( "timer"),
+    };
+
+    device!( MyDevice = device; 1024 );
+}
+
