ec4b598577c7debc80744e28d650b5af0f9f0a31
Ulf Lilleengen <ulf.lilleengen@gmail.com>
2021-02-02 08:00:26 +0100

    Update api

--- src/hal/uart/mod.rs
+++ src/hal/uart/mod.rs
@@ -7,14 +7,36 @@
 pub mod nrf;
 
 pub trait Uart {
+    /// Start a write operation to transmit the provided buffer.
     fn write_start(&mut self, tx_buffer: &[u8]) -> Result<(), Error>;
-    fn write_done(&mut self) -> bool;
+
+    /// Check progress of a write operation.
+    fn write_done(&self) -> bool;
+
+    /// Complete a write operation.
     fn write_finish(&mut self) -> Result<(), Error>;
+
+    /// Process interrupts for the peripheral. Implementations may need to use this to initiate
+    /// the next block of byte(s) to transfer.
+    fn process_interrupt(&mut self);
+
+    /// Start a read operation to receive data into rx_buffer.
+    fn read_start(&mut self, rx_buffer: &mut [u8]) -> Result<(), Error>;
+
+    /// Check progress of a write operation.
+    fn read_done(&self) -> bool;
+
+    /// Complete a read operation.
+    fn read_finish(&mut self) -> Result<usize, Error>;
+
+    /// Cancel a read operation
+    fn read_cancel(&mut self) -> Result<(), Error>;
 }
 
 #[derive(Debug, Clone)]
 pub enum Error {
     TxInProgress,
+    RxInProgress,
     TxBufferTooSmall,
     RxBufferTooSmall,
     TxBufferTooLong,
