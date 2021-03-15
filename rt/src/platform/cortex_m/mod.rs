446c3284c98185f658283d34951fc2acff5f4e3b
Ulf Lilleengen <ulf.lilleengen@gmail.com>
2021-03-15 14:04:06 +0100

    Add initial support for STM32L0XX

--- rt/src/platform/cortex_m/mod.rs
+++ rt/src/platform/cortex_m/mod.rs
@@ -1,8 +1,11 @@
 #[cfg(any(feature = "stm32l4xx", feature = "chip+stm32l4xx"))]
 pub mod stm32l4xx;
 
-#[cfg(any(feature = "stm32l1xx"))]
+#[cfg(any(feature = "chip+stm32l1xx"))]
 pub mod stm32l1xx;
+
+#[cfg(any(feature = "chip+stm32l0xx"))]
+pub mod stm32l0xx;
 
 #[cfg(any(feature = "chip+nrf51", feature = "chip+nrf52833",))]
 pub mod nrf;
---
+++ rt/src/platform/cortex_m/stm32l0xx/gpio.rs
@@ -0,0 +1,51 @@
+use crate::hal::gpio::InterruptPin;
+
+impl<P, E> InterruptPin for Pin<P, E>
+where
+    P: embedded_hal::digital::v2::InputPin,
+    E: stm32l0xx_hal::exti::ExtiLine + Copy,
+{
+    fn enable_interrupt(&mut self) {}
+    fn check_interrupt(&mut self) -> bool {
+        stm32l0xx_hal::exti::Exti::is_pending(self.line)
+    }
+
+    fn clear_interrupt(&mut self) {
+        stm32l0xx_hal::exti::Exti::unpend(self.line)
+    }
+}
+
+impl<P, E> embedded_hal::digital::v2::InputPin for Pin<P, E>
+where
+    P: embedded_hal::digital::v2::InputPin,
+    E: stm32l0xx_hal::exti::ExtiLine + Copy,
+{
+    type Error = P::Error;
+
+    fn is_high(&self) -> core::result::Result<bool, Self::Error> {
+        self.pin.is_high()
+    }
+
+    fn is_low(&self) -> core::result::Result<bool, Self::Error> {
+        self.pin.is_low()
+    }
+}
+
+pub struct Pin<P, E>
+where
+    P: embedded_hal::digital::v2::InputPin,
+    E: stm32l0xx_hal::exti::ExtiLine + Copy,
+{
+    pin: P,
+    line: E,
+}
+
+impl<P, E> Pin<P, E>
+where
+    P: embedded_hal::digital::v2::InputPin,
+    E: stm32l0xx_hal::exti::ExtiLine + Copy,
+{
+    pub fn new(pin: P, line: E) -> Self {
+        Self { pin, line }
+    }
+}
