b0ecff5183b35da9f02200f689e8e927235044a4
Jim Crossley <jim@crossleys.org>
2021-03-24 22:14:33 -0400

    Use button to join and avoid stm32f4-esp8266 on_start race condition
    The latest release of stm32f4xx-hal didn't include check_interrupt()
    in its 'ExtiPin' trait. And its master branch HEAD was incompatible
    with the latest nucleo BSP, so I got rid of the nucleo dep to bring in
    the latest HAL. This finally enabled the join to work in response to a
    button press.

--- examples/stm32f4/esp8266/src/device.rs
+++ examples/stm32f4/esp8266/src/device.rs
@@ -1,20 +1,25 @@
 use crate::app::*;
 use drogue_device::{
     domain::time::duration::Milliseconds,
-    driver::{timer::*, uart::serial::*, wifi::esp8266::Esp8266Wifi},
+    driver::{
+        button::{Button, ButtonEvent},
+        timer::*,
+        uart::serial::*,
+        wifi::esp8266::Esp8266Wifi,
+    },
     prelude::*,
 };
 
-use nucleo_f401re::{
-    hal::{
-        gpio::{
-            gpioc::{PC10, PC12},
-            Output, PushPull,
-        },
-        serial::{Rx, Tx},
+use hal::{
+    gpio::{
+        gpioc::{PC10, PC12, PC13},
+        Input, Output, PullUp, PushPull,
     },
-    pac::USART6,
+    serial::{Rx, Tx},
+    stm32::USART6,
 };
+
+use stm32f4xx_hal as hal;
 
 // TODO:
 pub type AppTimer = Timer<DummyTimer>;
@@ -23,8 +28,10 @@ type AppUart = Serial<Tx<USART6>, Rx<USART6>, <AppTimer as Package>::Primary>;
 type Wifi =
     Esp8266Wifi<<AppUart as Package>::Primary, PC10<Output<PushPull>>, PC12<Output<PushPull>>>;
 type AppWifi = <Wifi as Package>::Primary;
+type ButtonInterrupt = Button<MyDevice, PC13<Input<PullUp>>>;
 
 pub struct MyDevice {
+    pub button: InterruptContext<ButtonInterrupt>,
     pub timer: AppTimer,
     pub uart: AppUart,
     pub wifi: Wifi,
@@ -32,11 +39,18 @@ pub struct MyDevice {
 }
 
 impl Device for MyDevice {
-    fn mount(&'static self, _config: DeviceConfiguration<Self>, supervisor: &mut Supervisor) {
+    fn mount(&'static self, config: DeviceConfiguration<Self>, supervisor: &mut Supervisor) {
+        self.button.mount(config.event_bus, supervisor);
         let timer = self.timer.mount((), supervisor);
         let uart = self.uart.mount(timer, supervisor);
         let wifi = self.wifi.mount(uart, supervisor);
         self.app.mount(wifi, supervisor);
+    }
+}
+
+impl EventHandler<ButtonEvent> for MyDevice {
+    fn on_event(&'static self, event: ButtonEvent) {
+        self.app.address().notify(event);
     }
 }
 
--- examples/stm32f4/esp8266/src/main.rs
+++ examples/stm32f4/esp8266/src/main.rs
@@ -1,6 +1,11 @@
 #![no_main]
 #![no_std]
 
+const WIFI_SSID: &str = include_str!("wifi.ssid.txt");
+const WIFI_PSK: &str = include_str!("wifi.password.txt");
+const ENDPOINT: IpAddress = IpAddress::new_v4(192, 168, 1, 2);
+const ENDPOINT_PORT: u16 = 12345;
+
 mod app;
 mod device;
 
@@ -8,39 +13,36 @@ use panic_rtt_target as _;
 
 use cortex_m_rt::{entry, exception};
 use drogue_device::{
-    api::ip::IpAddress, driver::uart::serial::*, driver::wifi::esp8266::Esp8266Wifi, prelude::*,
+    api::ip::IpAddress,
+    driver::uart::serial::*,
+    driver::{button::Button, wifi::esp8266::Esp8266Wifi},
+    hal::Active,
+    prelude::*,
 };
 use log::LevelFilter;
 use rtt_logger::RTTLogger;
 use rtt_target::rtt_init_print;
 
-use nucleo_f401re::{
-    hal,
-    hal::{
-        prelude::*,
-        serial::{
-            config::{Config, Parity, StopBits},
-            Serial as NucleoSerial,
-        },
-    },
+use hal::gpio::Edge;
+use hal::serial::{
+    config::{Config, Parity, StopBits},
+    Serial as NucleoSerial,
 };
+use stm32f4xx_hal as hal;
+use stm32f4xx_hal::gpio::ExtiPin;
+use stm32f4xx_hal::prelude::*;
 
 use crate::app::*;
 use crate::device::*;
 
 static LOGGER: RTTLogger = RTTLogger::new(LevelFilter::Info);
 
-const WIFI_SSID: &str = include_str!("wifi.ssid.txt");
-const WIFI_PSK: &str = include_str!("wifi.password.txt");
-const ENDPOINT: IpAddress = IpAddress::new_v4(192, 168, 1, 2);
-const ENDPOINT_PORT: u16 = 12345;
-
 fn configure() -> MyDevice {
     rtt_init_print!();
     log::set_logger(&LOGGER).unwrap();
     log::set_max_level(log::LevelFilter::Info);
 
-    let device = hal::stm32::Peripherals::take().unwrap();
+    let mut device = hal::stm32::Peripherals::take().unwrap();
 
     let rcc = device.RCC.constrain();
     let clocks = rcc.cfgr.sysclk(84.mhz()).freeze();
@@ -81,9 +83,18 @@ fn configure() -> MyDevice {
     let uart = Serial::new(tx, rx, hal::stm32::Interrupt::USART6);
     let wifi = Esp8266Wifi::new(en, reset);
 
-    let timer = AppTimer::new(DummyTimer {}, hal::interrupt::TIM2);
+    let timer = AppTimer::new(DummyTimer {}, hal::stm32::Interrupt::TIM2);
+
+    // Create a button input with an interrupt
+    let mut button = gpioc.pc13.into_pull_up_input();
+    button.make_interrupt_source(&mut device.SYSCFG.constrain());
+    button.enable_interrupt(&mut device.EXTI);
+    button.trigger_on_edge(&mut device.EXTI, Edge::FALLING);
+
+    let button = Button::new(button, Active::Low);
 
     MyDevice {
+        button: InterruptContext::new(button, hal::stm32::Interrupt::EXTI15_10).with_name("button"),
         timer,
         uart,
         wifi,
@@ -91,6 +102,7 @@ fn configure() -> MyDevice {
             .with_name("app"),
     }
 }
+
 #[entry]
 fn main() -> ! {
     device!(MyDevice = configure; 12000);
