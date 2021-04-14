acd279511db1fdeeec3bd861741dc446a357dd36
Ulf Lilleengen <ulf.lilleengen@gmail.com>
2021-04-14 12:42:47 +0200

    Add some module documentation

--- device/src/lib.rs
+++ device/src/lib.rs
@@ -5,13 +5,88 @@
 #![feature(impl_trait_in_bindings)]
 #![feature(generic_associated_types)]
 #![feature(type_alias_impl_trait)]
-
+//! An async, no-alloc actor framework for embedded devices.
+//!
+//! See [the book](https://book.drogue.io/drogue-device/dev/index.html) for more about the architecture, how to write device drivers, and running some examples.
+//!
+//! # Actor System
+//!
+//! An _actor system_ is a framework that allows for isolating state within narrow contexts, making it easier to reason about system.
+//! Within a actor system, the primary component is an _Actor_, which represents the boundary of state usage.
+//! Each actor has exclusive access to its own state and only communicates with other actors through message-passing.
+//!
+//! # Example
+//!
+//! ```
+//! #![macro_use]
+//! #![allow(incomplete_features)]
+//! #![feature(generic_associated_types)]
+//! #![feature(min_type_alias_impl_trait)]
+//! #![feature(impl_trait_in_bindings)]
+//! #![feature(type_alias_impl_trait)]
+//! #![feature(concat_idents)]
+//!
+//! use drogue_device::*;
+//!
+//! pub struct MyActor {
+//!     name: &'static str,
+//! }
+//!
+//! pub struct SayHello<'m>(&'m str);
+//!
+//! impl MyActor {
+//!     pub fn new(name: &'static str) -> Self {
+//!         Self { name }
+//!     }
+//! }
+//!
+//! impl Actor for MyActor {
+//!     type Configuration = ();
+//!     type Message<'a> = SayHello<'a>;
+//!     type OnStartFuture<'a> = impl core::future::Future<Output = ()> + 'a;
+//!     type OnMessageFuture<'a> = impl core::future::Future<Output = ()> + 'a;
+//!
+//!     fn on_start(self: core::pin::Pin<&'_ mut Self>) -> Self::OnStartFuture<'_> {
+//!         async move { println!("[{}] started!", self.name) }
+//!     }
+//!
+//!     fn on_message<'m>(
+//!         self: core::pin::Pin<&'m mut Self>,
+//!         message: &'m mut Self::Message<'m>,
+//!     ) -> Self::OnMessageFuture<'m> {
+//!         async move {
+//!             println!("[{}] Hello {}", self.name, message.0);
+//!         }
+//!     }
+//! }
+//!
+//! #[derive(drogue::Device)]
+//! pub struct MyDevice {
+//!     a: ActorState<'static, MyActor>,
+//! }
+//!
+//! #[drogue::configure]
+//! fn configure() -> MyDevice {
+//!     MyDevice {
+//!         a: ActorState::new(MyActor::new("a")),
+//!     }
+//! }
+//!
+//! #[drogue::main]
+//! async fn main(mut context: DeviceContext<MyDevice>) {
+//!     let a_addr = context.device().a.mount(());
+//!     context.start();
+//!     a_addr.send(&mut SayHello("World")).await;
+//! }
+//!```
+//!
 pub use drogue_device_kernel::{
     actor::{Actor, ActorState, Address},
     channel::{consts, Channel},
     device::{Device, DeviceContext},
     package::{ImmediateFuture, Package},
 };
+#[doc(hidden)]
 pub use drogue_device_macros as drogue;
 pub use embassy::*;
 
--- kernel/src/actor.rs
+++ kernel/src/actor.rs
@@ -6,26 +6,49 @@ use core::pin::Pin;
 use core::task::{Context, Poll};
 use embassy::util::DropBomb;
 
+/// Trait that each actor must implement.
 pub trait Actor {
+    /// The configuration that this actor will expect when mounted.
     type Configuration;
+
+    /// The message type that this actor will handle in `on_message`.
     type Message<'a>: Sized
     where
         Self: 'a;
+
+    /// The future type returned in `on_start`, usually derived from an `async move` block
+    /// in the implementation
     type OnStartFuture<'a>: Future<Output = ()>
     where
         Self: 'a;
+
+    /// The future type returned in `on_message`, usually derived from an `async move` block
+    /// in the implementation
     type OnMessageFuture<'a>: Future<Output = ()>
     where
         Self: 'a;
 
+    /// Called to mount an actor into the system.
+    ///
+    /// The actor will be presented with both its own `Address<...>`.
+    ///
+    /// The default implementation does nothing.
     fn on_mount(&mut self, _: Self::Configuration) {}
+
+    /// Lifecycle event of *start*.
     fn on_start(self: Pin<&'_ mut Self>) -> Self::OnStartFuture<'_>;
+
+    /// Handle an incoming message for this actor.
     fn on_message<'m>(
         self: Pin<&'m mut Self>,
         message: &'m mut Self::Message<'m>,
     ) -> Self::OnMessageFuture<'m>;
 }
 
+/// A handle to another actor for dispatching messages.
+///
+/// Individual actor implementations may augment the `Address` object
+/// when appropriate bounds are met to provide method-like invocations.
 pub struct Address<'a, A: Actor> {
     state: &'a ActorState<'a, A>,
 }
@@ -37,6 +60,15 @@ impl<'a, A: Actor> Address<'a, A> {
 }
 
 impl<'a, A: Actor> Address<'a, A> {
+    /// Perform an unsafe _async_ message send to the actor behind this address.
+    ///
+    /// The returned future will be driven to completion by the actor processing the message.
+    ///
+    /// # Panics
+    /// While the request message may contain non-static references, the user must
+    /// ensure that the response to the request is fully `.await`'d before returning.
+    /// Leaving an in-flight request dangling while references have gone out of lifetime
+    /// scope will result in a panic.
     pub fn send<'m>(&self, message: &'m mut A::Message<'m>) -> SendFuture<'a, 'm, A> {
         self.state.send(message)
     }
@@ -66,6 +98,7 @@ impl<'a, A: Actor> ActorState<'a, A> {
         }
     }
 
+    /// Launch the actor main processing loop that never returns.
     pub async fn start(&'static self, _: embassy::executor::Spawner)
     where
         A: Unpin,
@@ -85,6 +118,7 @@ impl<'a, A: Actor> ActorState<'a, A> {
         }
     }
 
+    /// Acquire a signal slot if there are any free available
     fn acquire_signal(&self) -> &SignalSlot {
         let signals = unsafe { &mut *self.signals.get() };
         let mut i = 0;
@@ -100,7 +134,6 @@ impl<'a, A: Actor> ActorState<'a, A> {
     /// Send a message to this actor. The returned future _must_ be awaited before dropped. If it is not
     /// awaited, it will panic.
     fn send<'m>(&'a self, message: &'m mut A::Message<'m>) -> SendFuture<'a, 'm, A>
-    // impl Future<Output = ()> + 'a
     where
         A: 'm + 'a,
     {
@@ -112,6 +145,7 @@ impl<'a, A: Actor> ActorState<'a, A> {
         SendFuture::new(chan, sig)
     }
 
+    /// Mount the underloying actor and initialize the channel.
     pub fn mount(&'a self, config: A::Configuration) -> Address<'a, A> {
         unsafe { &mut *self.actor.get() }.on_mount(config);
         self.channel.initialize();
