3f44c0528c002301a2befa03a00faf6a8b810b77
Ulf Lilleengen <ulf.lilleengen@gmail.com>
2021-05-15 23:21:06 +0200

    Better document Package API

--- device/src/kernel/package.rs
+++ device/src/kernel/package.rs
@@ -1,8 +1,19 @@
 use super::actor::{Actor, ActorSpawner, Address};
 
+/// The package trait provides a way to bundle one or more actors and
+/// additional state in a package that can be used by other components.
+///
+/// A Package is mounted with its desired configuration, and has a primary
+/// Actor that it provides the Address of when mounted.
 pub trait Package {
+    /// The primary Actor for this package.
     type Primary: Actor;
+
+    /// The expected configuration when mounting.
     type Configuration = ();
+
+    /// Mount this package, providing the configuration and a reference
+    /// to a spawner used when mounting internal actors of the Package.
     fn mount(
         &'static self,
         config: Self::Configuration,
