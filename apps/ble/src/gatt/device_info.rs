9f48aa1063d7736a51211f2af826d9e8a8bb401a
Ulf Lilleengen <ulf.lilleengen@gmail.com>
2021-10-07 10:16:31 +0200

    Add missing file

---
+++ apps/ble/src/gatt/device_info.rs
@@ -0,0 +1,13 @@
+use heapless::Vec;
+
+#[nrf_softdevice::gatt_server(uuid = "0000180a-0000-1000-8000-00805f9b34fb")]
+pub struct DeviceInformationService {
+    #[characteristic(uuid = "00002a24-0000-1000-8000-00805f9b34fb", read)]
+    pub model_number: Vec<u8, 32>,
+    #[characteristic(uuid = "00002a25-0000-1000-8000-00805f9b34fb", read)]
+    pub serial_number: Vec<u8, 32>,
+    #[characteristic(uuid = "00002a27-0000-1000-8000-00805f9b34fb", read)]
+    pub hardware_revision: Vec<u8, 4>,
+    #[characteristic(uuid = "00002a29-0000-1000-8000-00805f9b34fb", read)]
+    pub manufacturer_name: Vec<u8, 32>,
+}
