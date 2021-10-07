51e45e1860680b0c895b240d8a7a3c5d231be291
Ulf Lilleengen <ulf.lilleengen@gmail.com>
2021-10-07 15:21:06 +0200

    Use cargo check

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -50,7 +50,7 @@ fn test_ci() -> Result<(), anyhow::Error> {
     ] {
         let mut example_dir = examples_dir.clone();
         example_dir.push(example);
-        test_example(example_dir)?;
+        check_example(example_dir)?;
     }
     Ok(())
 }
@@ -91,10 +91,10 @@ fn do_examples<F: FnMut(PathBuf) -> Result<(), anyhow::Error>>(
     Ok(())
 }
 
-fn test_example(project_file: PathBuf) -> Result<(), anyhow::Error> {
+fn check_example(project_file: PathBuf) -> Result<(), anyhow::Error> {
     println!("Building example {}", project_file.to_str().unwrap_or(""));
-    let _p = xshell::pushd(project_file)?; //.parent().unwrap())?;
-    cmd!("cargo build --release").run()?;
+    let _p = xshell::pushd(project_file)?;
+    cmd!("cargo check").run()?;
     Ok(())
 }
 
