1b17f99261f5e66bc29c76e20bfe293a3134a8ba
Ulf Lilleengen <lulf@redhat.com>
2021-10-11 13:04:06 +0200

    Update to new BLE gatt API

--- apps/ble/src/gatt/device_info.rs
+++ apps/ble/src/gatt/device_info.rs
@@ -1,6 +1,6 @@
 use heapless::Vec;
 
-#[nrf_softdevice::gatt_server(uuid = "0000180a-0000-1000-8000-00805f9b34fb")]
+#[nrf_softdevice::gatt_service(uuid = "0000180a-0000-1000-8000-00805f9b34fb")]
 pub struct DeviceInformationService {
     #[characteristic(uuid = "00002a24-0000-1000-8000-00805f9b34fb", read)]
     pub model_number: Vec<u8, 32>,
