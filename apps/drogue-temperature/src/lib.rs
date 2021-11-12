85e95be45ffadae70f41488de52c66084e01a957
Ulf Lilleengen <lulf@redhat.com>
2021-11-12 12:50:10 +0100

    Don't serialize none values

--- apps/drogue-temperature/src/lib.rs
+++ apps/drogue-temperature/src/lib.rs
@@ -22,8 +22,11 @@ pub struct GeoLocation {
 
 #[derive(Clone, Serialize, Deserialize)]
 pub struct TemperatureData {
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub geoloc: Option<GeoLocation>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub temp: Option<f32>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub hum: Option<f32>,
 }
 
