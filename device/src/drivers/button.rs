fa1693ed24d19026759fed0b09859676ee0057c6
Bob McWhirter <bmcwhirt@redhat.com>
2021-11-24 11:26:00 -0500

    Add a button trait to support maybe future BSP work.

--- device/src/drivers/button.rs
+++ device/src/drivers/button.rs
@@ -1,3 +1,4 @@
+use core::future::Future;
 use embassy::traits::gpio::WaitForAnyEdge;
 use embedded_hal::digital::v2::InputPin;
 
@@ -9,22 +10,44 @@ impl<P: WaitForAnyEdge + InputPin + 'static> Button<P> {
     pub fn new(pin: P) -> Self {
         Self { pin }
     }
+}
 
-    pub async fn wait_pressed(&mut self) {
-        loop {
-            self.pin.wait_for_any_edge().await;
-            if self.pin.is_low().ok().unwrap() {
-                break;
-            };
+impl<P: WaitForAnyEdge + InputPin + 'static> crate::traits::button::Button for Button<P> {
+    type WaitPressed<'m>
+    where
+        Self: 'm,
+    = impl Future<Output = ()> + 'm;
+
+    type WaitReleased<'m>
+    where
+        Self: 'm,
+    = impl Future<Output = ()> + 'm;
+
+    fn wait_pressed<'m>(&'m mut self) -> Self::WaitPressed<'m>
+    where
+        Self: 'm,
+    {
+        async move {
+            loop {
+                self.pin.wait_for_any_edge().await;
+                if self.pin.is_low().ok().unwrap() {
+                    break;
+                };
+            }
         }
     }
 
-    pub async fn wait_released(&mut self) {
-        loop {
-            self.pin.wait_for_any_edge().await;
-            if self.pin.is_high().ok().unwrap() {
-                break;
-            };
+    fn wait_released<'m>(&'m mut self) -> Self::WaitReleased<'m>
+    where
+        Self: 'm,
+    {
+        async move {
+            loop {
+                self.pin.wait_for_any_edge().await;
+                if self.pin.is_high().ok().unwrap() {
+                    break;
+                };
+            }
         }
     }
 }
