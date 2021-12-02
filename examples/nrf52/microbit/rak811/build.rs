1b1764770dbf5e10039250960ce13adcb81b2086
Jim Crossley <jim@crossleys.org>
2021-12-02 16:48:03 -0500

    Clean up build scripts
    Removed the now minimally-useful `examples/_build` crate
    
    Removed the build.rs files that aren't needed to successfully run
    `cargo build --release`
    
    The remaining build.rs files are all identical to the one in the
    cortex-m quickstart: https://github.com/rust-embedded/cortex-m-quickstart/blob/master/build.rs

--- examples/nrf52/microbit/rak811/build.rs
+++ examples/nrf52/microbit/rak811/build.rs
@@ -8,12 +8,24 @@
 //! updating `memory.x` ensures a rebuild of the application with the
 //! new memory settings.
 
-use buildutil::copy_file;
 use std::env;
+use std::fs::File;
+use std::io::Write;
 use std::path::PathBuf;
 
 fn main() {
-    copy_file("memory.x");
+    // Put `memory.x` in our output directory and ensure it's
+    // on the linker search path.
     let out = &PathBuf::from(env::var_os("OUT_DIR").unwrap());
+    File::create(out.join("memory.x"))
+        .unwrap()
+        .write_all(include_bytes!("memory.x"))
+        .unwrap();
     println!("cargo:rustc-link-search={}", out.display());
+
+    // By default, Cargo will re-run a build script whenever
+    // any file in the project changes. By specifying `memory.x`
+    // here, we ensure the build script is only re-run when
+    // `memory.x` is changed.
+    println!("cargo:rerun-if-changed=memory.x");
 }
