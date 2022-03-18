bb9b95e114aaa91efcd2586e02384f5cd468cb98
Bob McWhirter <bmcwhirt@redhat.com>
2022-03-18 15:23:50 -0400

    Make the transmute more sound. Adjust the example a tad.

--- examples/nrf52/adafruit-nrf-feather/neopixel/src/main.rs
+++ examples/nrf52/adafruit-nrf-feather/neopixel/src/main.rs
@@ -6,9 +6,7 @@
 
 #[cfg(feature = "defmt-rtt")]
 use defmt_rtt as _;
-use drogue_device::drivers::led::neopixel::{
-    CyclicBrightness, Filter, Gamma, NeoPixel, Rgb8, BLUE, GREEN, RED,
-};
+use drogue_device::drivers::led::neopixel::{CyclicBrightness, Filter, Gamma, NeoPixel, BLUE};
 use embassy::executor::Spawner;
 use embassy::time::{Duration, Timer};
 use embassy_nrf::Peripherals;
@@ -18,16 +16,16 @@ use panic_probe as _;
 #[cfg(not(feature = "panic-probe"))]
 use panic_reset as _;
 
-const STEP_SIZE: u8 = 8;
+const STEP_SIZE: u8 = 2;
 
 #[embassy::main]
 async fn main(_spawner: Spawner, p: Peripherals) {
     let mut neopixel = defmt::unwrap!(NeoPixel::<'_, _, 1>::new(p.PWM0, p.P0_16));
 
-    let cyclic = CyclicBrightness::new(16, 127, STEP_SIZE);
+    let cyclic = CyclicBrightness::new(64, 127, STEP_SIZE);
     let mut filter = cyclic.and(Gamma);
     loop {
         neopixel.set_with_filter(&[BLUE], &mut filter).await.ok();
-        Timer::after(Duration::from_millis(15)).await;
+        Timer::after(Duration::from_millis(20)).await;
     }
 }
