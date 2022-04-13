26eba39f1ea5c526c8e442a9c800903d9f342317
Ulf Lilleengen <lulf@redhat.com>
2022-04-13 08:29:15 +0200

    Remove unused actors

--- device/src/actors/mod.rs
+++ device/src/actors/mod.rs
@@ -3,12 +3,6 @@ pub mod ble;
 //
 //
 pub mod button;
-//pub mod flash;
-//pub mod i2c;
 pub mod led;
-//pub mod lora;
-//pub mod net;
 pub mod sensors;
-//pub mod tcp;
-//pub mod wifi;
 pub mod transformer;
