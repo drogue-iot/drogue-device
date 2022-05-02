5609e8d67cb6df2e8160f5d39313a9b3fc1ea33d
Ondrej Babec <ond.babec@gmail.com>
2022-05-02 13:39:59 +0200

    Format mqtt

--- examples/nrf52/microbit/esp8266/mqtt/src/main.rs
+++ examples/nrf52/microbit/esp8266/mqtt/src/main.rs
@@ -13,15 +13,11 @@ use panic_probe as _;
 
 use drogue_device::traits::button::Button;
 
-use drogue_device::{
-    drivers::dns::*,
-    bsp::boards::nrf52::microbit::*,
-    traits::ip::*,
-    network::clients::mqtt::*,
-    *,
-};
-use drogue_device::{drivers::wifi::esp8266::*};
 use drogue_device::bsp::boards::nrf52::microbit::LedMatrix;
+use drogue_device::drivers::wifi::esp8266::*;
+use drogue_device::{
+    bsp::boards::nrf52::microbit::*, drivers::dns::*, network::clients::mqtt::*, traits::ip::*, *,
+};
 use embassy::time::{Duration, Timer};
 use embassy_nrf::{
     gpio::{Level, Output, OutputDrive},
@@ -32,20 +28,22 @@ use embassy_nrf::{
     Peripherals,
 };
 
-use rust_mqtt::client::client_config::MqttVersion::MQTTv5;
-use rust_mqtt::{
-    client::{client_config::ClientConfig, client::MqttClient},
-    packet::v5::publish_packet::QualityOfService,
+use drogue_device::network::connection::{
+    ConnectionFactory, TlsConnectionFactory, TlsNetworkConnection,
 };
-use rust_mqtt::network::NetworkConnection;
-use rust_mqtt::utils::rng_generator::CountingRng;
-use drogue_device::network::connection::{ConnectionFactory, TlsConnectionFactory, TlsNetworkConnection};
 use drogue_device::network::socket::Socket;
 use drogue_device::network::tcp::TcpStackState;
+use drogue_device::shared::Handle;
 use drogue_device::traits::dns::DnsResolver;
 use drogue_device::traits::tcp::TcpStack;
 use drogue_device::traits::wifi::{Join, WifiSupplicant};
-use drogue_device::shared::Handle;
+use rust_mqtt::client::client_config::MqttVersion::MQTTv5;
+use rust_mqtt::network::NetworkConnection;
+use rust_mqtt::utils::rng_generator::CountingRng;
+use rust_mqtt::{
+    client::{client::MqttClient, client_config::ClientConfig},
+    packet::v5::publish_packet::QualityOfService,
+};
 
 const WIFI_SSID: &str = drogue::config!("wifi-ssid");
 const WIFI_PSK: &str = drogue::config!("wifi-password");
@@ -100,27 +98,31 @@ async fn main(spawner: embassy::executor::Spawner, p: Peripherals) {
     static NETWORK: TcpStackState<Esp8266Controller<'static, TX>> = TcpStackState::new();
     let mut network = NETWORK.initialize(network);
 
-
     let mut conn_factory = {
         static mut TLS_BUFFER: [u8; 16384] = [0; 16384];
         static mut TLS_BUFFER_SEC: [u8; 16384] = [0; 16384];
-        TlsConnectionFactory::<'static, _ ,Aes128GcmSha256, _, 2>::new(network.clone(), Rng::new(nrf52833_pac::Peripherals::take().unwrap().RNG), unsafe { [&mut TLS_BUFFER, &mut TLS_BUFFER_SEC] })
+        TlsConnectionFactory::<'static, _, Aes128GcmSha256, _, 2>::new(
+            network.clone(),
+            Rng::new(nrf52833_pac::Peripherals::take().unwrap().RNG),
+            unsafe { [&mut TLS_BUFFER, &mut TLS_BUFFER_SEC] },
+        )
     };
 
     let ips = DNS.resolve(HOST).await.expect("unable to resolve host");
 
-
     defmt::info!("Creating sockets");
 
-    let connection_pub = conn_factory.connect(HOST, ips[0], PORT.parse::<u16>().unwrap()).await.unwrap();
-    let connection_recv = conn_factory.connect(HOST, ips[0], PORT.parse::<u16>().unwrap()).await.unwrap();
+    let connection_pub = conn_factory
+        .connect(HOST, ips[0], PORT.parse::<u16>().unwrap())
+        .await
+        .unwrap();
+    let connection_recv = conn_factory
+        .connect(HOST, ips[0], PORT.parse::<u16>().unwrap())
+        .await
+        .unwrap();
 
     static RECEIVER: ActorContext<Receiver> = ActorContext::new();
-    RECEIVER.mount(
-        spawner,
-        Receiver::new(board.display, connection_recv),
-    );
-
+    RECEIVER.mount(spawner, Receiver::new(board.display, connection_recv));
 
     let mut config = ClientConfig::new(MQTTv5, CountingRng(0));
     config.add_qos(QualityOfService::QoS0);
@@ -147,19 +149,15 @@ async fn main(spawner: embassy::executor::Spawner, p: Peripherals) {
         defmt::info!("[PUBLISHER] sending message");
         client.send_message(TOPIC, "{'temp':42}").await.unwrap();
         defmt::info!("[PUBLISHER] message sent");
-
     }
 }
 
-static DNS: StaticDnsResolver<'static, 3> = StaticDnsResolver::new(&[
+static DNS: StaticDnsResolver<'static, 2> = StaticDnsResolver::new(&[
     DnsEntry::new("localhost", IpAddress::new_v4(127, 0, 0, 1)),
     DnsEntry::new(
-        "broker.emqx.io",
-        IpAddress::new_v4(100, 69, 164, 175),
-    ),     DnsEntry::new(
         "mqtt.sandbox.drogue.cloud",
         IpAddress::new_v4(65, 108, 135, 161),
-    )
+    ),
 ]);
 
 #[embassy::task]
@@ -167,14 +165,26 @@ pub async fn wifi(mut modem: Esp8266Modem<'static, RX, ENABLE, RESET>) {
     modem.run().await;
 }
 
-
 pub struct Receiver {
     display: LedMatrix,
-    connection: Option<TlsNetworkConnection<'static, Handle<'static, Esp8266Controller<'static, UarteTx<'static, UARTE0>>>, Aes128GcmSha256>>,
+    connection: Option<
+        TlsNetworkConnection<
+            'static,
+            Handle<'static, Esp8266Controller<'static, UarteTx<'static, UARTE0>>>,
+            Aes128GcmSha256,
+        >,
+    >,
 }
 
 impl Receiver {
-    pub fn new(display: LedMatrix, connection: TlsNetworkConnection<'static, Handle<'static, Esp8266Controller<'static, UarteTx<'static, UARTE0>>>, Aes128GcmSha256>) -> Self {
+    pub fn new(
+        display: LedMatrix,
+        connection: TlsNetworkConnection<
+            'static,
+            Handle<'static, Esp8266Controller<'static, UarteTx<'static, UARTE0>>>,
+            Aes128GcmSha256,
+        >,
+    ) -> Self {
         Self {
             display,
             connection: Some(connection),
@@ -192,42 +202,49 @@ impl Actor for Receiver {
     type Message<'m> = ReceiverMessage;
 
     async fn on_mount<M>(&mut self, _: Address<ReceiverMessage>, mut _inbox: M)
-        where
-            M: Inbox<Self::Message<'m>> + 'm,
+    where
+        M: Inbox<Self::Message<'m>> + 'm,
     {
-            let mut config = ClientConfig::new(MQTTv5, CountingRng(0));
-            config.add_qos(QualityOfService::QoS1);
-            config.add_username(USERNAME);
-            config.add_password(PASSWORD);
-            config.keep_alive = u16::MAX;
-            let mut recv_buffer = [0; 1000];
-            let mut write_buffer = [0; 1000];
+        let mut config = ClientConfig::new(MQTTv5, CountingRng(0));
+        config.add_qos(QualityOfService::QoS1);
+        config.add_username(USERNAME);
+        config.add_password(PASSWORD);
+        config.keep_alive = u16::MAX;
+        let mut recv_buffer = [0; 1000];
+        let mut write_buffer = [0; 1000];
 
-            let mut client = MqttClient::<TlsNetworkConnection<'_, Handle<'_, Esp8266Controller<'static, UarteTx<'static, UARTE0>>>, Aes128GcmSha256>, 20, CountingRng>::new(
-                self.connection.take().unwrap(),
-                &mut write_buffer,
-                1000,
-                &mut recv_buffer,
-                1000,
-                config,
-            );
-            defmt::info!("[RECEIVER] Connecting to broker!");
-            client.connect_to_broker().await.unwrap();
-            defmt::info!("[RECEIVER] Subscribing to topic!");
-            client.subscribe_to_topic(TOPIC_S).await;
-            loop {
-                defmt::info!("[RECEIVER] Waiting for new message");
-                let msg = client.receive_message().await;
-                if msg.is_ok() {
-                    let message = msg.unwrap();
-                    let act_message = core::str::from_utf8(message).unwrap();
-                    defmt::info!("[RECEIVER] Received: {}", act_message);
-                    self.display.scroll(act_message).await;
-                } else {
-                    defmt::warn!("[RECEIVER] Could not get message!");
-                }
-                Timer::after(Duration::from_secs(2)).await;
+        let mut client = MqttClient::<
+            TlsNetworkConnection<
+                '_,
+                Handle<'_, Esp8266Conroller<'static, UarteTx<'static, UARTE0>>>,
+                Aes128GcmSha256,
+            >,
+            20,
+            CountingRng,
+        >::new(
+            self.connection.take().unwrap(),
+            &mut write_buffer,
+            1000,
+            &mut recv_buffer,
+            1000,
+            config,
+        );
+        defmt::info!("[RECEIVER] Connecting to broker!");
+        client.connect_to_broker().await.unwrap();
+        defmt::info!("[RECEIVER] Subscribing to topic!");
+        client.subscribe_to_topic(TOPIC_S).await;
+        loop {
+            defmt::info!("[RECEIVER] Waiting for new message");
+            let msg = client.receive_message().await;
+            if msg.is_ok() {
+                let message = msg.unwrap();
+                let act_message = core::str::from_utf8(message).unwrap();
+                defmt::info!("[RECEIVER] Received: {}", act_message);
+                self.display.scroll(act_message).await;
+            } else {
+                defmt::warn!("[RECEIVER] Could not get message!");
             }
+            Timer::after(Duration::from_secs(2)).await;
+        }
     }
 }
-
