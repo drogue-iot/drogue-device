ed828dfd4c90ea082d0b4cf6780bd5e0b3d60e25
Ulf Lilleengen <lulf@redhat.com>
2022-06-16 10:19:51 +0200

    Remove warning

--- examples/stm32l4/iot01a/wifi/src/main.rs
+++ examples/stm32l4/iot01a/wifi/src/main.rs
@@ -2,6 +2,7 @@
 #![no_main]
 #![macro_use]
 #![allow(incomplete_features)]
+#![allow(unused_imports)]
 #![allow(dead_code)]
 #![feature(generic_associated_types)]
 #![feature(type_alias_impl_trait)]
