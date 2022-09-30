d01140148237abb59b8b6599eec01df223e2f028
Ulf Lilleengen <ulf.lilleengen@gmail.com>
2022-09-30 10:21:54 +0200

    Add async task for running accelerometer continuously

--- boards/microbit/src/accelerometer/mod.rs
+++ boards/microbit/src/accelerometer/mod.rs
@@ -4,10 +4,13 @@ use embassy_nrf::{
     peripherals::{P0_08, P0_16, TWISPI0},
     twim, Peripheral,
 };
+use embassy_sync::channel::DynamicSender;
+use embassy_time::{Duration, Ticker};
+use futures::StreamExt;
 use lsm303agr::{
-    interface::I2cInterface, mode::MagOneShot, AccelMode, AccelOutputDataRate, Error as LsmError,
-    Lsm303agr, Measurement, Status,
+    interface::I2cInterface, mode::MagOneShot, AccelMode, Error as LsmError, Lsm303agr, Status,
 };
+pub use lsm303agr::{AccelOutputDataRate, Measurement};
 
 type I2C<'d> = twim::Twim<'d, TWISPI0>;
 
@@ -32,7 +35,7 @@ impl<'d> Accelerometer<'d> {
 
         let mut sensor = Lsm303agr::new_with_i2c(twi);
         sensor.init()?;
-        sensor.set_accel_odr(AccelOutputDataRate::Hz50)?;
+        sensor.set_accel_odr(AccelOutputDataRate::Hz10)?;
         sensor.set_accel_mode(AccelMode::Normal)?;
 
         Ok(Self { sensor })
@@ -48,5 +51,31 @@ impl<'d> Accelerometer<'d> {
     /// Returned in mg (milli-g) where 1g is 9.8m/s².
     pub fn accel_data(&mut self) -> Result<Measurement, Error> {
         self.sensor.accel_data()
+    }
+
+    /// Run a continuous task outputing accelerometer data at the configured data rate
+    pub async fn run(
+        &mut self,
+        rate: AccelOutputDataRate,
+        sender: DynamicSender<'_, Measurement>,
+    ) -> Result<(), Error> {
+        let delay = match rate {
+            AccelOutputDataRate::Hz1 => Duration::from_millis(1000),
+            AccelOutputDataRate::Hz10 => Duration::from_millis(100),
+            AccelOutputDataRate::Hz25 => Duration::from_millis(40),
+            AccelOutputDataRate::Hz50 => Duration::from_millis(20),
+            AccelOutputDataRate::Hz100 => Duration::from_millis(10),
+            AccelOutputDataRate::Hz200 => Duration::from_millis(5),
+            AccelOutputDataRate::Hz400 => Duration::from_micros(2500),
+            AccelOutputDataRate::Khz1_344 => Duration::from_micros(744),
+            AccelOutputDataRate::Khz1_620LowPower => Duration::from_micros(617),
+            AccelOutputDataRate::Khz5_376LowPower => Duration::from_micros(186),
+        };
+        let mut ticker = Ticker::every(delay);
+        loop {
+            ticker.next().await;
+            let data = self.accel_data()?;
+            let _ = sender.try_send(data);
+        }
     }
 }
