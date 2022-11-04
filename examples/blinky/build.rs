25a9dc6bc660ac03d1cc1773dd1920748e471217
Ulf Lilleengen <ulf.lilleengen@gmail.com>
2022-11-04 18:34:11 +0100

    Remove unused dep

--- examples/blinky/build.rs
+++ examples/blinky/build.rs
@@ -1,14 +1,4 @@
-use std::{env, fs::File, io::Write, path::PathBuf};
-
 fn main() {
-    let out = &PathBuf::from(env::var_os("OUT_DIR").unwrap());
-    let mut v = Vec::new();
-    for (key, value) in env::vars() {
-        v.push(format!("{}={}", key, value));
-    }
-
-    println!("cargo:rustc-link-search={}", out.display());
-    //    println!("cargo:rerun-if-changed=memory.x");
     println!("cargo:rustc-link-arg-bins=--nmagic");
     println!("cargo:rustc-link-arg-bins=-Tlink.x");
     println!("cargo:rustc-link-arg-bins=-Tdefmt.x");
--- examples/metaboard/build.rs
+++ examples/metaboard/build.rs
@@ -1,9 +1,6 @@
-use std::{env, fs::File, io::Write, path::PathBuf};
-extern crate drogue_metaboard_gen;
-use drogue_metaboard_gen::*;
+use std::{env, path::PathBuf, fs::File, io::Write};
 
 fn main() {
-    let crate_dir = PathBuf::from(env::var_os("CARGO_MANIFEST_DIR").unwrap());
     let board_name = env::vars_os()
         .map(|(a, _)| a.to_string_lossy().to_string())
         .find(|x| x.starts_with("CARGO_FEATURE_BOARD+"))
@@ -17,4 +14,22 @@ fn main() {
     let in_memory_x = std::fs::read(data_dir.join("memory.x")).unwrap();
 
     gen_memory(&board_name, &in_memory_x[..]);
+}
+
+pub fn gen_memory(board_name: &str, in_memory_x: &[u8]) {
+    let out_dir = PathBuf::from(env::var_os("OUT_DIR").unwrap());
+
+    std::fs::create_dir_all(format!("{}/src/boards/{}", out_dir.display(), board_name)).unwrap();
+    let out_memory_x = format!("{}/src/boards/{}/memory.x", out_dir.display(), board_name);
+    File::create(&out_memory_x)
+        .unwrap()
+        .write_all(in_memory_x)
+        .unwrap();
+
+    println!("cargo:rerun-if-changed=build.rs");
+    println!(
+        "cargo:rustc-link-search={}/src/boards/{}",
+        out_dir.display(),
+        board_name
+    );
 }
